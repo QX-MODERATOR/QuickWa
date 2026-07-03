fa4bd95e0d7a7191bdf74ee2175bc2bd7e3dc564
Mohammad Kayyali <32301001140@std.bau.edu.jo>
2026-07-03 06:35:21 +0300

    Add app/src/main/java/com/quickwa/ui/theme/Color.kt

---
+++ app/src/main/java/com/quickwa/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.quickwa.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF0C7C5A)
+val GreenSecondary = Color(0xFF18A77A)
+val GreenTertiary = Color(0xFF2BC49A)
+
